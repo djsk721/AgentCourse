59bd51ad75c6482633281c14c9ec1804b9f43486
김정현 <52440551+djsk721@users.noreply.github.com>
2026-06-18 04:45:16 +0000

    feat: update

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-pip install -r requirements.txt
+#!/bin/bash
+set -e
 
-uvicorn company_portal:app --reload
+python -m pip install --upgrade pip
+pip install -r uvicorn fastapi
+
+python -m uvicorn company_portal:app --host 0.0.0.0 --port 8000
