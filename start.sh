f7727ab217f56b3166d216da0183c2737f9f946b
김정현 <52440551+djsk721@users.noreply.github.com>
2026-06-18 05:24:41 +0000

    test

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-python -m pip install --upgrade pip
-pip install fastapi uvicorn
-
-python -m gunicorn company_portal:app \
-  -k uvicorn.workers.UvicornWorker \
-  --bind 0.0.0.0:8000
+python -m uvicorn company_portal:app --host 0.0.0.0 --port 8000
